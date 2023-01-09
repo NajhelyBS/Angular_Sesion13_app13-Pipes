036e56cfe14e0c2e5bdf11e7af7ccdae163f2dd0
NajhelyBS <s5anbandasanchez@gmail.com>
2023-01-09 09:36:45 -0500

    uppercase works - still working in more

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebModule } from './web/web.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    WebModule,
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+  ],
+  exports:[
   ]
 })
 export class WebModule { }
